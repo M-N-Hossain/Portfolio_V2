6545c5270e65e5838753bb756c9af3398a753147
nhnahid1122 <mdxx0356@stud.kea.dk>
2024-11-29 21:29:45 +0100

    changes made in about me section

--- src/components/AboutME.jsx
+++ src/components/AboutME.jsx
@@ -11,15 +11,22 @@ export default function AboutME() {
         <h3>A dedicated programmer.</h3>
         <h3>Based in Copenhagen, Denmark 📍</h3>
         <p className="secText">
-          I'm a determined Bangladeshi fellow following my dream to be an efficient
-          software developer. With a passion for coding, I've honed my skills in
-          Java, JavaScript, and TypeScript and I'm well-equipped with frameworks
-          like Spring Boot, React, React Native, and Express.js in the Node
-          environment. The best qualities I have are being a quick learner,
-          a hardworking person, and dedicated toward my responsibilities.
-          Because of my unwavering passion for technology, I
-          am always ready to learn and explore technologies and my hardworking
-          mindset allows me to adapt and manage any situation openly.
+          {/* I'm a determined Bangladeshi fellow following my dream to be an
+          efficient software developer. With a passion for coding, I've honed my
+          skills in Java, JavaScript, and TypeScript and I'm well-equipped with
+          frameworks like Spring Boot, React, React Native, and Express.js in
+          the Node environment. The best qualities I have are being a quick
+          learner, a hardworking person, and dedicated toward my
+          responsibilities. I'm always ready to learn and explore new technologies
+          and my hardworking mindset allows me to adapt and manage any situation
+          openly. */}
+          I'm a determined Bangladeshi fellow following my dream of being an
+          efficient software developer, passionate about building efficient and
+          scalable web and mobile applications. I love to continuously learn and
+          explore new technologies which makes me confident in building and
+          optimizing end-to-end applications that serve both business and user
+          needs. My journey has been guided by curiosity, adaptability, and a
+          commitment to growth, which I apply to every project I take on.
         </p>
       </div>
       <RoundedText />
